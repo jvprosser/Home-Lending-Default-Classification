609dd3a238fb45abbfa57c0f4db6ff841be09480
Joseph Prosser <jprosser@cloudera.com>
2024-09-27 18:57:54 +0000

    removed quote issue

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-echo '{ "disabledExtensions”: { "@jupyter/collaboration-extension”: true }, "lockedExtensions": { "@jupyter/collaboration-extension": true } }' > /home/cdsw/.jupyter/labconfig/page_config.json
+echo '{ "disabledExtensions”: { "@jupyter/collaboration-extension": true }, "lockedExtensions": { "@jupyter/collaboration-extension": true } }' > /home/cdsw/.jupyter/labconfig/page_config.json
 hdfs dfs -get s3a://jqrhv9chljtm/data/loan-default-dataset.zip .
 unzip loan-default-dataset.zip
 pip install numpy pandas seaborn  matplotlib scikit-learn tabulate  cufflinks  plotly xgboost
